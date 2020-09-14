12b8c88048604a537c9cd5aed2654abbbb203cc5
Preeti <preetijav@gmail.com>
2020-09-14 11:28:43 +0200

    Page not found added

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,16 @@
 import React, { Fragment, useEffect } from 'react';
-import './App.css';
-import Navbar from './components/layout/Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Dashboard from './components/dashboard/Dashboard';
-import PrivateRoute from './components/routing/PrivateRoute';
-import CreateProfile from './components/profile-form/CreateProfile';
-import EditProfile from './components/profile-form/EditProfile';
-import AddExperience from './components/profile-form/AddExperience';
-import AddEducation from './components/profile-form/AddEducation';
-import Profiles from './components/profiles/Profiles';
-import Profile from './components/profile/Profile';
-import Posts from './components/posts/Posts';
-import Post from './components/post/Post';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import { Routes }  from './components/routing/Routes'
+
+import './App.css';
 
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
-
-import Register from './components/auth/register';
-import Login from './components/auth/login';
-import Landing from './components/layout/Landing';
-import './App.css';
-import Alert from './components/layout/Alert';
 import setAuthToken from './utils/setAuthToken';
-
 
 if(localStorage.token){
   setAuthToken(localStorage.token);
@@ -40,24 +26,10 @@ const App = () => {
     <Router>
     <Fragment>
     <Navbar/>
-    <Route exact path='/' component={Landing}/>
-    <section className="container">
-    <Alert/>
-      <Switch>
-        <Route path="/register" component={Register}/>
-        <Route path="/login" component={Login}/>
-        <PrivateRoute path="/dashboard" component={Dashboard}/>
-        <PrivateRoute path="/create-profile" component={CreateProfile}/>
-        <PrivateRoute path="/edit-profile" component={EditProfile}/>
-        <Route path="/profiles" component={Profiles}/>
-        <PrivateRoute path="/profile/:id" component={Profile}/>
-        <PrivateRoute path="/add-experience" component={AddExperience}/>
-        <PrivateRoute path="/add-education" component={AddEducation}/>
-        <PrivateRoute path="/posts" component={Posts}/> 
-        <PrivateRoute path="/post/:id" component={Post}/>
- 
-      </Switch>
-    </section>
+    <Switch/>
+      <Route exact path='/' component={Landing}/>
+      <Route component={Routes} />
+    <Switch/>
   </Fragment>
   </Router> 
   </Provider>
---
+++ client/src/components/routing/Routes.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Dashboard from '../dashboard/Dashboard';
+import PrivateRoute from '../routing/PrivateRoute';
+import CreateProfile from '../profile-form/CreateProfile';
+import EditProfile from '../profile-form/EditProfile';
+import AddExperience from '../profile-form/AddExperience';
+import AddEducation from '../profile-form/AddEducation';
+import Profiles from '../profiles/Profiles';
+import Profile from '../profile/Profile';
+import Posts from '../posts/Posts';
+import Post from '../post/Post';
+import NotFound  from '../layout/NotFound';
+import Register from '../auth/register';
+import Login from '../auth/login';
+import Alert from '../layout/Alert';
+
+
+export const Routes = () => {
+    return (
+        <section className="container">
+          <Alert/>
+          <Switch>
+            <Route path="/register" component={Register}/>
+            <Route path="/login" component={Login}/>
+            <PrivateRoute path="/dashboard" component={Dashboard}/>
+            <PrivateRoute path="/create-profile" component={CreateProfile}/>
+            <PrivateRoute path="/edit-profile" component={EditProfile}/>
+            <Route path="/profiles" component={Profiles}/>
+            <PrivateRoute path="/profile/:id" component={Profile}/>
+            <PrivateRoute path="/add-experience" component={AddExperience}/>
+            <PrivateRoute path="/add-education" component={AddEducation}/>
+            <PrivateRoute path="/posts" component={Posts}/> 
+            <PrivateRoute path="/post/:id" component={Post}/>
+            <Route component={NotFound}></Route> 
+          </Switch>
+        </section>
+    )
+}
