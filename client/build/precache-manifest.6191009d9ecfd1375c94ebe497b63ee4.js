c15e0c1afa929a1cb64e4ba470d44248d1e64b20
Preeti <preetijav@gmail.com>
2020-09-16 15:48:02 +0200

    ready for deployment

---
+++ client/build/precache-manifest.6191009d9ecfd1375c94ebe497b63ee4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e3c835e3657104b8cf1e9bbf8fb5b3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29fb321bdf01b5d04520",
+    "url": "/static/css/main.7d9d1bb1.chunk.css"
+  },
+  {
+    "revision": "e73026e6677813c71569",
+    "url": "/static/js/2.18f6c7b6.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/static/js/2.18f6c7b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29fb321bdf01b5d04520",
+    "url": "/static/js/main.5f8c0cbf.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
